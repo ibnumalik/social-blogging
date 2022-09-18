eabd9e9c01bc9488ee93a1eb21d8e6c5f63eb69a
Shaiful Aiman Malik <ibnumalikmy@gmail.com>
2022-09-18 15:30:34 +0800

    add user posts list page

--- nextjs-amplify/pages/_app.js
+++ nextjs-amplify/pages/_app.js
@@ -1,8 +1,32 @@
 import '../styles/globals.css';
 import '../configureAmplify';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { Auth, Hub } from 'aws-amplify';
 
 function MyApp({ Component, pageProps }) {
+  const [signedInUser, setSignedInUser] = useState(false);
+
+  useEffect(() => {
+    authListener();
+  });
+
+  async function authListener() {
+    Hub.listen('auth', (data) => {
+      switch (data.payload.event) {
+        case 'signIn':
+          return setSignedInUser(true);
+        case 'signOut':
+          return setSignedInUser(false);
+      }
+    });
+
+    try {
+      await Auth.currentAuthenticatedUser();
+      setSignedInUser(true);
+    } catch (error) {}
+  }
+
   return (
     <div>
       <nav className="p-6 border-b border-gray-300">
@@ -12,6 +36,11 @@ function MyApp({ Component, pageProps }) {
         <Link href="/create-post">
           <span className="mr-6 cursor-pointer">Create Post</span>
         </Link>
+        {signedInUser && (
+          <Link href="/my-posts">
+            <span className="mr-6 cursor-pointer">My Posts</span>
+          </Link>
+        )}
         <Link href="/profile">
           <span className="mr-6 cursor-pointer">Profile</span>
         </Link>
---
+++ nextjs-amplify/pages/my-posts.js
@@ -0,0 +1,36 @@
+import { API, Auth } from 'aws-amplify';
+import { useEffect, useState } from 'react';
+import { postsByUsername } from '../graphql/queries';
+
+export default function MyPosts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const { username } = await Auth.currentAuthenticatedUser();
+    const postData = await API.graphql({
+      query: postsByUsername,
+      variables: { username },
+    });
+    setPosts(postData.data.postsByUsername.items);
+  }
+
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold tracking-wide mt-6 mb-2">
+        My Posts
+      </h1>
+      {posts.map((post, index) => (
+        <Link key={index} href={`/posts/${post.id}`}>
+          <div className="cursor-pointer border-b border-gray-300	mt-8 pb-4">
+            <h2 className="text-xl font-semibold">{post.title}</h2>
+            <p className="text-gray-500 mt-2">Author: {post.username}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
