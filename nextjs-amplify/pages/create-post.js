e7be4f1322365511722742634e32198d68c844a1
Shaiful Aiman Malik <ibnumalikmy@gmail.com>
2022-09-19 13:30:50 +0800

    accept cover image in creating post

--- nextjs-amplify/pages/create-post.js
+++ nextjs-amplify/pages/create-post.js
@@ -1,8 +1,9 @@
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import { useCallback, useState } from 'react';
-import { API } from 'aws-amplify';
+import { useCallback, useRef, useState } from 'react';
+import { API, Storage } from 'aws-amplify';
 import { v4 as uuid } from 'uuid';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import SimpleMDEEditor from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { createPost } from '../graphql/mutations';
@@ -11,17 +12,28 @@ const initialState = { title: '', content: '' };
 
 function CreatePost() {
   const [post, setPost] = useState(initialState);
+  const [image, setImage] = useState(null);
+  const hiddenFileInput = useRef(null);
   const { title, content } = post;
   const router = useRouter();
 
-  const onChange = useCallback((e) => {
-    setPost(() => ({ ...post, [e.target.name]: e.target.value }));
-  })
+  const onChange = useCallback(
+    (e) => {
+      setPost(() => ({ ...post, [e.target.name]: e.target.value }));
+    },
+    [post]
+  );
 
   async function createNewPost() {
     if (!title || !content) return;
     const id = uuid();
     post.id = id;
+
+    if (image) {
+      const fileName = `${image.name}_${uuid()}`;
+      post.coverImage = fileName;
+      await Storage.put(fileName, image);
+    }
 
     await API.graphql({
       query: createPost,
@@ -30,6 +42,16 @@ function CreatePost() {
     });
 
     router.push(`/posts/${id}`);
+  }
+
+  function uploadImage() {
+    hiddenFileInput.current.click();
+  }
+
+  function handleChange(e) {
+    const fileUploaded = e.target.files[0];
+    if (!fileUploaded) return;
+    setImage(fileUploaded);
   }
 
   return (
@@ -44,10 +66,39 @@ function CreatePost() {
         value={post.title}
         className="border-b pb-2 text-lg my-4 focus:outline-none w-full font-light text-gray-500 placeholder-gray-500 y-2"
       />
+
+      {image && (
+        <div className="h-[400px] relative">
+          <Image
+            alt={post.title + ' cover image'}
+            src={URL.createObjectURL(image)}
+            className="my-4"
+            width="100%"
+            height="400px"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center center"
+          />
+        </div>
+      )}
       <SimpleMDEEditor
         value={post.content}
         onChange={(value) => setPost({ ...post, content: value })}
       />
+      <div className="cover-image">
+        <input
+          type="file"
+          ref={hiddenFileInput}
+          className="absolute w-0 h-0"
+          onChange={handleChange}
+        />
+        <button
+          onClick={uploadImage}
+          className="bg-purple-600 text-white font-semibold px-8 py-2 rounded-lg mr-2 mb-4"
+        >
+          Upload cover image
+        </button>
+      </div>
       <button
         type="button"
         className="mb-4 bg-blue-600 text-white font-semibold px-8 py-2 rounded-lg"
