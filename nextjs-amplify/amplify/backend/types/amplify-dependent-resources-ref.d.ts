19a757d11a730f6021b06f64f55b2564406f108f
Shaiful Aiman Malik <ibnumalikmy@gmail.com>
2022-09-19 12:58:26 +0800

    update schema to add cover image for post

--- nextjs-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ nextjs-amplify/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "SocialBlogPostImages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
