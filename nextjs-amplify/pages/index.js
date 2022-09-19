e953c0cc81e1035e66c44fb418fefd20ec83bb53
Shaiful Aiman Malik <ibnumalikmy@gmail.com>
2022-09-19 14:10:02 +0800

    show thumbnail in homepage

--- nextjs-amplify/pages/index.js
+++ nextjs-amplify/pages/index.js
@@ -1,4 +1,4 @@
-import { API } from 'aws-amplify';
+import { API, Storage } from 'aws-amplify';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { listPosts } from '../graphql/queries';
@@ -12,7 +12,17 @@ export default function Home() {
 
   async function fetchPosts() {
     const postData = await API.graphql({ query: listPosts });
-    setPosts(postData.data.listPosts.items);
+    const { items } = postData.data.listPosts;
+    const postsWithImages = await window.Promise.all(
+      items.map(async (post) => {
+        if (post.coverImage) {
+          post.coverImage = await Storage.get(post.coverImage);
+        }
+
+        return post;
+      })
+    );
+    setPosts(postsWithImages);
   }
 
   return (
@@ -20,11 +30,14 @@ export default function Home() {
       <h1 className="text-3xl font-semibold tracking-wide mt-6 mb-2">Posts</h1>
       {posts.map((post, index) => (
         <Link key={index} href={`/posts/${post.id}`}>
-          <div className="cursor-pointer border-b border-gray-300 mt-8 pb-4">
-            <h2 className="text-xl font-semibold">{post.title}</h2>
-            <p className="text-gray-500 mt-2">
-              Author: {post.username || 'Anonymous'}
-            </p>
+          <div className="border-b border-gray-300 my-6 pb-6">
+            {post.coverImage && <img src={post.coverImage} className="w-56" />}
+            <div className="cursor-pointer mt-2">
+              <h2 className="text-xl font-semibold">{post.title}</h2>
+              <p className="text-gray-500 mt-2">
+                Author: {post.username || 'Anonymous'}
+              </p>
+            </div>
           </div>
         </Link>
       ))}
